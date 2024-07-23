955428eb430d4d52728dceea6961eeb7a8a0f3b3
ranjith-bb <ranjithbb282005@gmail.com>
2024-07-23 21:40:28 +0530

    Create Add Two Numbers.py

---
+++ 23.07.2024/Add Two Numbers.py
@@ -0,0 +1,6 @@
+def add_two_numbers():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    print(f"The sum is: {num1 + num2}")
+
+add_two_numbers()
