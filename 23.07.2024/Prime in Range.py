dc33095b9b311531d4ee632adf9d95a95c4043e7
ranjith-bb <ranjithbb282005@gmail.com>
2024-07-23 21:38:28 +0530

    Create Prime in Range.py

---
+++ 23.07.2024/Prime in Range.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def primes_in_range(start, end):
+    return [num for num in range(start, end + 1) if is_prime(num)]
+
+start = 10
+end = 50
+print(f"Prime numbers in range: {primes_in_range(start, end)}")
