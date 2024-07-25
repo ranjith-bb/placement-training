7c77ff16b662c9d0a5d9b1362581a5dbd1db6f1c
ranjith-bb <ranjithbb282005@gmail.com>
2024-07-25 06:29:29 +0530

    Create Maximum of Two Number.java

---
+++ 24.07.2024/Maximum of Two Number.java
@@ -0,0 +1,8 @@
+public class Maximum {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int max = (a > b) ? a : b;
+        System.out.println("Maximum: " + max);
+    }
+}
